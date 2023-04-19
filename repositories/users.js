27ed03c2c8921d2a05a960b31c084498218969b7
Codeprince001 <urangyorkwisdom@gmail.com>
2023-04-19 13:14:43 +0100

    created a create func to created a user info for each user and a randomId func to generate randomID for each user ifo

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
   constructor(filename) {
@@ -12,6 +13,38 @@ class UsersRepository {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
+  async getAll() {
+    return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
+  }
+
+  // create user record
+  async create(attribute) {
+    // add random id to the each user info
+    attribute.id = this.randomId();
+
+    const records = await this.getAll();
+    records.push(attribute);
+
+    this.writeAll(records);
+  }
+  // write user info in file.json
+  async writeAll(records) {
+    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+  }
+  // generate random id for each user info
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
 }
 
-new UsersRepository('users.json');
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+
+  await repo.create({ email: 'tested@testing.com', password: 'password' });
+
+  users = await repo.getAll();
+  console.log(users);
+};
+
+test();
