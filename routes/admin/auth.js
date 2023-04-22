bf855fdee97d161b41c20151982dd22975597fa3
Codeprince001 <urangyorkwisdom@gmail.com>
2023-04-22 16:02:39 +0100

    added express-validator depency
    to validate email, and password of user during a post request

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -10,7 +11,13 @@ router.get('/signup', (req, res) => {
 });
 
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', [
+  check('email').trim().normalizeEmail().isEmail(),
+  check('password').trim().isLength({ min: 4, max: 20 }),
+  check('passwordConfirmation').trim().isLength({ min: 4, max: 20 })
+], async (req, res) => {
+  const errors = validationResult(req);
+  console.log(errors);
   const { email, password, passwordConfirmation } = req.body;
 
   const existingUser = await usersRepo.getOneBy({ email });
