c6d7c4f4994bd31a2bb4ecd78057e1509681e94c
Codeprince001 <urangyorkwisdom@gmail.com>
2023-04-25 21:14:20 +0100

    products route handlers

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const productRepo = require('../../repositories/products');
+const newProductTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validator');
+const { validationResult } = require('express-validator');
+
 
 const router = express.Router();
 
 router.get('/admin/products', (req, res) => {
 
+
 });
 
 router.get('/admin/products/new', (req, res) => {
+  res.send(newProductTemplate({}));
+});
+
+router.post('/admin/products/new',
+  [requireTitle, requirePrice],
+  async (req, res) => {
+
+    const errors = validationResult(req);
+    console.log(errors);
+    res.send('Submitted');
+  });
+
+
+
+router.post('',);
+
+router.post('/admin/products/new', async (req, res) => {
 
 });
 
