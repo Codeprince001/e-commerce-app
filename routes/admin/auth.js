4d89caccc63285745b46ac2d4762bd519c235691
Codeprince001 <urangyorkwisdom@gmail.com>
2023-04-27 16:32:34 +0100

    Added authentication for accessing route

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
+
+
+const { errorHandler } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -14,21 +16,15 @@ router.get('/signup', (req, res) => {
 
 router.post('/signup',
   [requireEmail, requirePassword, requireConfirmPassword],
+  errorHandler(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors);
-    // errors : [{value: 'password', msg: ''password invalid, param: 'password', location: 'body'}] 
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     // create a user in our repo to represent this person
     const user = await usersRepo.create({ email, password });
     // Store the id of that user inside the users cookie
     req.session.userId = user.id;
 
-    res.send("Account Created");
+    res.redirect('/admin/products');
   });
 
 router.get('/signout', (req, res) => {
@@ -42,19 +38,16 @@ router.get('/signin', (req, res) => {
 });
 
 router.post('/signin',
-  [requireExactEmail, requireExactPassword], async (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors);
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ req, errors }));
-    }
+  [requireExactEmail, requireExactPassword],
+  errorHandler(signinTemplate),
+  async (req, res) => {
 
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
     req.session.userId = user.id;
 
-    res.send('You are signed in!!!!');
+    res.redirect('/admin/products');
   });
 
 module.exports = router;
--- routes/admin/product.js
+++ routes/admin/product.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const multer = require('multer');
-const { validationResult } = require('express-validator');
 
+const { errorHandler, requireAuth } = require('./middlewares');
 const newProductTemplate = require('../../views/admin/products/new');
+const productIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validator');
 const productRepo = require('../../repositories/products');
 
@@ -11,30 +12,28 @@ const productRepo = require('../../repositories/products');
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {
-
+router.get('/admin/products', requireAuth, async (req, res) => {
+  const products = await productRepo.getAll();
+  return res.send(productIndexTemplate({ products }));
 
 });
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
   res.send(newProductTemplate({}));
 });
 
-router.post('/admin/products/new',
+router.post('/admin/products/new', requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
+  errorHandler(newProductTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      res.send(newProductTemplate({ errors }));
-    }
     const image = req.file.buffer.toString('base64');
     const { title, price } = req.body;
 
     await productRepo.create({ title, price, image });
 
-    res.send('Submitted');
+    res.redirect('/admin/products');
   });
 
 
