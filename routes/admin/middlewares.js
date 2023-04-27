2afe5dba97828c40bd68f997a105ff16a85c6df8
Codeprince001 <urangyorkwisdom@gmail.com>
2023-04-27 16:31:16 +0100

    created file to handle custom middleware

---
+++ routes/admin/middlewares.js
@@ -0,0 +1,20 @@
+const { validationResult } = require('express-validator');
+
+module.exports = {
+  errorHandler(templateFunc) {
+    return (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.send(templateFunc({ errors }));
+      }
+      next();
+    };
+  },
+  requireAuth(req, res, next) {
+    if (!req.session.userId) {
+      res.redirect('/signin');
+    }
+    next();
+  }
+};
