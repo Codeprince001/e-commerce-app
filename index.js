5de24d904c7b691a3fe77de77b2a92f1fb7c4805
Codeprince001 <urangyorkwisdom@gmail.com>
2023-04-27 15:07:44 +0100

    fix typo

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/product');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
   name: 'session',
   keys: ['jkhad5jbwh9k6nxbhfcbwheg6']
 }));
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('listening');
-}); 
+});
