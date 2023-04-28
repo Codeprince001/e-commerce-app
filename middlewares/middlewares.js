60d4b85ff26b1b3c16908c991a0c421888731038
Codeprince001 <urangyorkwisdom@gmail.com>
2023-04-28 13:50:39 +0100

    modified middleware
     the error handler simplifies the code by providing a central location to handle errors in a consistent way for multiple routes, and allows for easy customization of error pages based on the specific route and error messages.

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,12 +1,23 @@
 const { validationResult } = require('express-validator');
 
 module.exports = {
-  errorHandler(templateFunc) {
-    return (req, res, next) => {
+  /* When this error handler middleware is used in a route handler,
+  it first checks if there are any errors from the previous middleware,
+  using the validationResult function from the express-validator package.
+  If there are errors, the dataCb function is called (if provided)to fetch 
+  any necessary data for the template, and then the error message(s) and the fetched data 
+  are passed to the templateFunc to generate an error page to be sent as the response.*/
+
+  errorHandler(templateFunc, dataCb) {
+    return async (req, res, next) => {
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
-        return res.send(templateFunc({ errors }));
+        let data = {};
+        if (dataCb) {
+          data = await dataCb(req);
+        }
+        return res.send(templateFunc({ errors, ...data }));
       }
       next();
     };
