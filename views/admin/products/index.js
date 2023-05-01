d399115208016f3d745f97d3a96bc55099ad5ab9
Codeprince001 <urangyorkwisdom@gmail.com>
2023-05-01 13:00:37 +0100

    rendered right template

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -4,55 +4,45 @@ module.exports = ({ products }) => {
   const renderedProducts = products
     .map(product => {
       return `
-        <div class="column is-one-quarter">
-          <div class="card product-card">
-            <figure>
-              <img src="data:image/png;base64, ${product.image}"/>
-            </figure>
-            <div class="card-content">
-              <h3 class="subtitle">${product.title}</h3>
-              <h5>$${product.price}</h5>
-            </div>
-            <footer class="card-footer">
-              <form action="/cart/products" method="POST">
-                <input hidden value="${product.id}" name="productId"/>
-                <button class="button has-icon is-inverted">
-                  <i class="fa fa-shopping-cart"></i> Add to cart
-                </button>
-              </form>
-            </footer>
-          </div>
-        </div>
-      `;
+      <tr>
+        <td>${product.title}</td>
+        <td>${product.price}</td>
+        <td>
+          <a href="/admin/products/${product.id}/edit">
+            <button class="button is-link">
+              Edit
+            </button>
+          </a>
+        </td>
+        <td>
+          <form method="POST" action="/admin/products/${product.id}/delete">
+            <button class="button is-danger">Delete</button>
+          </form>
+        </td>
+      </tr>
+    `;
     })
-    .join('\n');
+    .join('');
 
   return layout({
     content: `
-      <section class="banner">
-        <div class="container">
-          <div class="columns is-centered">
-            <img src="/images/banner.jpg" />
-          </div>
-        </div>
-      </section>
-      
-      <section>
-        <div class="container">
-          <div class="columns">
-            <div class="column "></div>
-            <div class="column is-four-fifths">
-              <div>
-                <h2 class="title text-center">Featured Items</h2>
-                <div class="columns products">
-                  ${renderedProducts}  
-                </div>
-              </div>
-            </div>
-            <div class="column "></div>
-          </div>
-        </div>
-      </section>
+      <div class="control">
+        <h1 class="subtitle">Products</h1>  
+        <a href="/admin/products/new" class="button is-primary">New Product</a>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${renderedProducts}
+        </tbody>
+      </table>
     `
   });
 };
